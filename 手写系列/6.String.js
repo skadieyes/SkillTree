3fd46336abfe3c954c7a7231ef6aa7dbd3a9702b
zhengranqiu <zhengranqiu@maoyan.com>
2020-03-09 20:17:51 +0800

    feat： String.prototype

--- 手写系列/6.String.js
+++ 手写系列/6.String.js
@@ -41,3 +41,27 @@ console.log('The index of the 2nd "' + searchTerm + '" is ' + paragraph.indexOf(
 //lastIndexOf() 方法返回调用String 对象的指定值最后一次出现的索引，在一个字符串中的指定位置 fromIndex处从后向前搜索。如果没找到这个特定值则返回-1 。
 'canal'.lastIndexOf('a');     // returns 3 
 
+// match
+// match方法检索返回一个字符串匹配正则表达式的的结果，会返回一个数组
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var regexp = /[A-E]/gi;
+var matches_array = str.match(regexp);
+
+console.log(matches_array);
+// ['A', 'B', 'C', 'D', 'E', 'a', 'b', 'c', 'd', 'e']
+
+// replace 
+// str.replace(regexp|substr, newSubStr|function)
+var str = 'Twas the night before Xmas...';
+var newstr = str.replace(/xmas/i, 'Christmas');
+console.log(newstr);  // Twas the night before Christmas...
+
+// search
+// 当你想要知道字符串中是否存在某个模式（pattern）时可使用 search()，类似于正则表达式的 test() 方法。当要了解更多匹配信息时，可使用 match()（但会更慢一些），该方法类似于正则表达式的 exec() 方法。
+var str = "hey JudE";
+var re = /[A-Z]/g;
+var re2 = /[.]/g;
+console.log(str.search(re)); // returns 4, which is the index of the first capital letter "J"
+console.log(str.search(re2)); // returns -1 cannot find '.' dot punctuation
+
+// 
