dfebcb74eeba4bc6a8083a26f1e31e0e6605420c
郑染秋 <zhengranqiu@bilibili.com>
2020-07-13 13:34:57 +0800

    2020-7-13

---
+++ 编码/函数式编程/1.useCurry.js
@@ -0,0 +1,54 @@
+var curry = require('lodash').curry;
+
+// 函数用curry包一下食用更佳
+var match = curry(function(what, str) {
+  return str.match(what);
+});
+
+var replace = curry(function(what, replacement, str) {
+  return str.replace(what, replacement);
+});
+
+var filter = curry(function(f, ary) {
+  return ary.filter(f);
+});
+
+var map = curry(function(f, ary) {
+  return ary.map(f);
+});
+
+
+// using
+
+match(/\s+/g, "hello world");
+// [ ' ' ]
+
+match(/\s+/g)("hello world");
+// [ ' ' ]
+
+var hasSpaces = match(/\s+/g);
+// function(x) { return x.match(/\s+/g) }
+
+hasSpaces("hello world");
+// [ ' ' ]
+
+hasSpaces("spaceless");
+// null
+
+filter(hasSpaces, ["tori_spelling", "tori amos"]);
+// ["tori amos"]
+
+var findSpaces = filter(hasSpaces);
+// function(xs) { return xs.filter(function(x) { return x.match(/\s+/g) }) }
+
+findSpaces(["tori_spelling", "tori amos"]);
+// ["tori amos"]
+
+var noVowels = replace(/[aeiou]/ig);
+// function(replacement, x) { return x.replace(/[aeiou]/ig, replacement) }
+
+var censored = noVowels("*");
+// function(x) { return x.replace(/[aeiou]/ig, "*") }
+
+censored("Chocolate Rain");
+// 'Ch*c*l*t* R**n'
