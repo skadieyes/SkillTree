cae0e19d01feae56ccc5557e9e8a2d34d4f65145
郑染秋 <zhengranqiu@bilibili.com>
2020-07-24 12:58:16 +0800

    2020-7-24

---
+++ 编码/函数式编程/2.compose.js
@@ -0,0 +1,42 @@
+// 让代码从右向左运行，而不是由内而外运行
+var toUpperCase = function(x) { return x.toUpperCase(); };
+var exclaim = function(x) { return x + '!'; };
+var shout = compose(exclaim, toUpperCase);
+
+shout("send in the clowns");
+//=> "SEND IN THE CLOWNS!"
+
+
+// pointfree
+// Pointfree style means never having to say your data
+// 非 pointfree，因为提到了数据：word
+var snakeCase = function (word) {
+  return word.toLowerCase().replace(/\s+/ig, '_');
+};
+
+// pointfree
+var snakeCase = compose(replace(/\s+/ig, '_'), toLowerCase);
+
+
+// debug
+// 可以用trace函数来追踪函数的执行情况
+var trace = curry(function(tag, x){
+  console.log(tag, x);
+  return x;
+});
+
+var dasherize = compose(join('-'), toLower, split(' '), replace(/\s{2,}/ig, ' '));
+
+dasherize('The world is a vampire');
+// TypeError: Cannot read property 'apply' of undefined
+
+
+var dasherize = compose(join('-'), toLower, trace("after split"), split(' '), replace(/\s{2,}/ig, ' '));
+// after split [ 'The', 'world', 'is', 'a', 'vampire' ]
+
+
+var dasherize = compose(join('-'), map(toLower), split(' '), replace(/\s{2,}/ig, ' '));
+
+dasherize('The world is a vampire');
+
+// 'the-world-is-a-vampire'
