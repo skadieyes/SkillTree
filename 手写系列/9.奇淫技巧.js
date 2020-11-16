2fe397f6b8ee390b021215e376df166b92b97ed4
skadi <zhengranqiu@bilibili.com>
2020-11-16 15:52:14 +0800

    2020-11-16

--- 手写系列/9.奇淫技巧.js
+++ 手写系列/9.奇淫技巧.js
@@ -2,4 +2,18 @@
 const arr = Array.apply(null, new Array(4));
 arr.map((elem, index) => index);
 
+// 相等比较
+Object.is(0 , ' '); //false
+Object.is(null, undefined); //false
+Object.is([1], true); //false
+Object.is(NaN, NaN); //true
+
+// 获取文件后缀名
+function getFileExtension1(filename) {
+  return (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename)[0] : undefined;
+}
+
+function getFileExtension3(filename) {
+  return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+}
 // 
